c2d55cfbc7e29b5a08822a0880730875a8bdf6b9
PakyoPaez <pep.ponz.sanchez@gmail.com>
2026-03-13 11:31:59 +0100

    yea

--- src/main.py
+++ src/main.py
@@ -136,8 +136,6 @@ def ejecutar_proyecto_completo() -> None:
 # Punto de entrada estándar de Python.
 # Este bloque SOLO se ejecuta cuando lanzas el script directamente:
 #   python main.py
-# pito cum cum
-# estoy super del chill porque he renunciado al azucar
 # NO se ejecuta si otro módulo importa main (ej: import main).
 # =============================================================================
 if __name__ == "__main__":
